cdd742debfe07bba9d4c95c46bc0d4367e65f702
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-15 16:23:26 +0200

    Empty Compressor and Decompressor classes

---
+++ huffman/src/main/java/com/example/Compressor.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+public class Compressor {
+    
+
+    public void compress(String filePath, int unitSize)
+    {
+        
+    }
+}
---
+++ huffman/src/main/java/com/example/Decompressor.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Decompressor {
+    
+
+    
+    public void decompress(String filePath)
+    {
+        
+    }
+}
